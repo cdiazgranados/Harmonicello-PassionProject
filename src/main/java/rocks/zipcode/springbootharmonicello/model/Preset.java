e616e991a3c1a2c888ce05bf75204e05b8897062
Carolina Diazgranados <linacello@gmail.com>
2023-01-08 19:17:23 -0500

    added readme

--- src/main/java/rocks/zipcode/springbootharmonicello/model/Preset.java
+++ src/main/java/rocks/zipcode/springbootharmonicello/model/Preset.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 @Entity(name = "presets")
 public class Preset {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private Integer hertz;
